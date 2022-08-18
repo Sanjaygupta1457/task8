1eba997ea527608fb392038767eac4b64c2e72ff
Sanjaygupta1457 <sanjaygupta1457@gmail.com>
2022-08-18 17:06:21 +0530

    task9

--- js.js
+++ js.js
@@ -1,7 +1,11 @@
 var form = document.getElementById('addform');
 var Items = document.getElementById('items');
+var filter= document.getElementById('filter');
+
 
 form.addEventListener('submit' , addItem);
+//filter evevnt
+filter.addEventListener('keyup' , filterItems);
 
 function addItem(e){
     e.preventDefault();
@@ -17,4 +21,22 @@ var Deletebtn = document.createElement('button');
 Deletebtn.appendChild(document.createTextNode('X'));
 list.appendChild(Deletebtn);
 Items.appendChild(list);
+}
+  
+//filter item
+function filterItems(e){
+//converts text to lowercase
+var text = e.target.value.toLowerCase();
+//get lis
+var items= Items.getElementsByTagName('li');
+//conver to an array
+Array.from(items).forEach(function(item){
+ var itemName= item.firstChild.textContent;
+ if(itemName.toLowerCase.indexOf(text)!=-1){
+     item.style.display='block';   
+ }
+ else{
+    item.style.display = 'none' ;
+ }
+});
 }
